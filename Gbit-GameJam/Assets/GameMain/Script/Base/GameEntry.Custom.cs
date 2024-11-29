6a593222e44fde5d0dde826b059624bdeed1b301
Superppig <272378634@qq.com>
2024-11-29 22:38:27 +0800

    修复了EnemyManager脚本名字错误问题,将EnemyManager注册到框架中,修改了斧头ui和预制体贴图

--- Gbit-GameJam/Assets/GameMain/Script/Base/GameEntry.Custom.cs
+++ Gbit-GameJam/Assets/GameMain/Script/Base/GameEntry.Custom.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public partial class GameEntry : MonoBehaviour
 {
-    
-    
+    /// <summary>
+    /// 敌人管理器
+    /// </summary>
+    public static EnemyManager EnemyManager { get; private set; }
+
     private void InitCustomComponents()
     {
         // 将来在这里注册自定义的组件
+        EnemyManager = UnityGameFramework.Runtime.GameEntry.GetComponent<EnemyManager>();
     }
 
     private void InitCustomDebuggers()
